3966ac1d5cd4fa29c012db37ef78cf6fa3b2c3d0
PixelGuy75 <crafterguy75@gmail.com>
2015-08-15 17:34:32 -0500

    Additions and fixes

--- src/staffonly/Main.php
+++ src/staffonly/Main.php
@@ -9,6 +9,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 use pocketmine\network\protocol\LoginPacket;
+use pocketmine\permission\Permissible;
 
 class Main extends PluginBase implements Listener {
     
@@ -22,7 +23,7 @@ class Main extends PluginBase implements Listener {
     
     public function onDataPacketReceive(DataPacketReceiveEvent $event) {
         if($event->getPacket() instanceof LoginPacket){
-            if($this->mode === true && !$event->getPlayer()->isOp()){
+            if($this->mode === true && !$event->getPlayer()->hasPermission("staffonly.allow")){
                 $event->setCancelled(true);
                 $event->getPlayer()->close("", $this->getConfig()->get("quit-message"));
             }
@@ -31,16 +32,15 @@ class Main extends PluginBase implements Listener {
     
     public function enableStaff(CommandSender $sender) {
         $this->mode = true;
-        $sender->sendMessage("Enabled StaffOnly");
+        $sender->sendMessage(TextFormat::GREEN."Enabled StaffOnly");
     }
     
     public function disableStaff(CommandSender $sender) {
         $this->mode = false;
-        $sender->sendMessage("Disabled StaffOnly");
+        $sender->sendMessage(TextFormat::RED."Disabled StaffOnly");
     }
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-        if($sender->isOp()){
             switch($command->getName()){
                 case "staffonlyon":
                     $this->enableStaff($sender);
@@ -49,6 +49,5 @@ class Main extends PluginBase implements Listener {
                     $this->disableStaff($sender);
                     break;
             }
-        }
     }
 }   
