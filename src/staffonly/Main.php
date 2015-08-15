380851e2e9a4df2a839fe11e96e6346e7273c570
Victor T. <pro_victor@ymail.com>
2015-08-15 13:12:17 -0700

    Update Main.php

--- src/staffonly/Main.php
+++ src/staffonly/Main.php
@@ -4,28 +4,38 @@ namespace staffonly;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\Player;
-use pocketmine\player\PlayerJoinEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
+use pocketmine\network\protocol\LoginPacket;
 
 class Main extends PluginBase implements Listener {
     
     public function onEnable() {
+        $this->mode = false;
+        if($this->getConfig()->get("mode") === true){
+            $this->mode = true;
+        }
         $this->getLogger()->info(TextFormat::GOLD."Staff Only Enabled");
     }
     
-    public function onJoin(PlayerJoinEvent $event) {
-        
+    public function onDataPacketReceive(DataPacketReceiveEvent $event) {
+        if($event->getPacket() instanceof LoginPacket){
+            if($this->mode === true && !$event->getPlayer()->isOp()){
+                $event->setCancelled(true);
+                $event->getPlayer()->close("", $this->getConfig()->get("quit-message"));
+            }
+        }
     }
     
-    public function enableStaff($sender) {
+    public function enableStaff(CommandSender $sender) {
+        $this->mode = true;
         $sender->sendMessage("Enabled StaffOnly");
     }
     
-    public function disableStaff($sender) {
+    public function disableStaff(CommandSender $sender) {
+        $this->mode = false;
         $sender->sendMessage("Disabled StaffOnly");
     }
     
