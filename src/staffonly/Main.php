f5d1e857e8217f97c86e6c775210764f1948d44b
PixelGuy75 <crafterguy75@gmail.com>
2015-08-16 13:54:24 -0500

    Fixes

--- src/staffonly/Main.php
+++ src/staffonly/Main.php
@@ -4,16 +4,17 @@ namespace staffonly;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\Player;
 use pocketmine\utils\TextFormat;
-use pocketmine\network\protocol\LoginPacket;
 use pocketmine\permission\Permissible;
 
 class Main extends PluginBase implements Listener {
     
     public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->mode = false;
         if($this->getConfig()->get("mode") === true){
             $this->mode = true;
@@ -21,18 +22,21 @@ class Main extends PluginBase implements Listener {
         $this->getLogger()->info(TextFormat::GOLD."Staff Only Enabled");
     }
     
-    public function onDataPacketReceive(DataPacketReceiveEvent $event) {
-        if($event->getPacket() instanceof LoginPacket){
-            if($this->mode === true && !$event->getPlayer()->hasPermission("staffonly.allow")){
-                $event->setCancelled(true);
-                $event->getPlayer()->close("", $this->getConfig()->get("quit-message"));
-            }
+    public function onLogin(PlayerLoginEvent $event) {
+        if($this->mode === true && !$event->getPlayer()->hasPermission("staffonly.allow")) {
+            $event->setCancelled(true);
+            $event->getPlayer()->close("", $this->getConfig()->get("quit-message"));
         }
     }
     
     public function enableStaff(CommandSender $sender) {
         $this->mode = true;
         $sender->sendMessage(TextFormat::GREEN."Enabled StaffOnly");
+        foreach($this->getServer()->getOnlinePlayers() as $players){
+            if(!$players->hasPermission("staffonly.allow")) {
+                $players->close("", $this->getConfig()->get("quit-message"));
+            }
+        }
     }
     
     public function disableStaff(CommandSender $sender) {
